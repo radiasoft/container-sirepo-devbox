cc64736a550aacec5ef172c600e1fdc16f4952d9
Evan Carlin <evan@carlin.com>
2023-06-15 14:46:10 -0600

    Fix #3: install openssh-server (#4)

--- container-conf/build.sh
+++ container-conf/build.sh
@@ -3,5 +3,5 @@ build_image_base=${sirepo_devbox_base:-radiasoft/sirepo-ci}
 build_is_public=1
 
 build_as_root() {
-    build_yum install rscode-geant4
+    build_yum install openssh-server rscode-geant4
 }
