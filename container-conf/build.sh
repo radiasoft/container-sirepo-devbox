3d3d94043bb4ca2c3f69441fbe99b1500cd076e3
e-carlin <evan@carlin.com>
2023-08-07 21:47:50 +0000

    pr comments

--- container-conf/build.sh
+++ container-conf/build.sh
@@ -13,8 +13,4 @@ build_as_root() {
 
 build_as_run_user() {
     install_repo_eval container-sirepo-base
-    install_url radiasoft/sirepo
-    #POSIT: This relies on the fact that individual package names don't have spaces or special chars
-    npm install -g \
-        $(install_download package.json | jq -r '.devDependencies | to_entries | map("\(.key)@\(.value)") | .[]')
 }
