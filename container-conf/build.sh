0db4613eec38470269ad5183c46ad8376acbdddf
Evan Carlin <evan@carlin.com>
2023-09-12 16:33:37 -0600

    Fix #9: openssh-server now supplied by redhat-base (#10)

--- container-conf/build.sh
+++ container-conf/build.sh
@@ -4,7 +4,7 @@ build_image_base=${sirepo_devbox_base:-radiasoft/beamsim-jupyter}
 
 build_as_root() {
     install_repo_eval container-sirepo-base root
-    build_yum install openssh-server rscode-geant4
+    build_yum install rscode-geant4
     # QT5 expects kernel >= 3.15. Centos7 supports 3.10. This removes
     # the kernel check when loading the lib. This could result in
     # errors when using QT but so far we haven't experienced any.
